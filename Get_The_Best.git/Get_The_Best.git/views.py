df0a556feb9a7ba9c780b3ea929a1255e06e29c0
dasarisasidhar <dasarisasidhar.d@gmail.com>
2019-12-09 14:34:42 +0530

    venv

--- Get_The_Best.git/Get_The_Best.git/views.py
+++ Get_The_Best.git/Get_The_Best.git/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from Get_The_Best.git import app
+from Get_The_Best import app
 
 @app.route('/')
 @app.route('/home')
